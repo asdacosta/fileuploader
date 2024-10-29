d0f71ba754d7bd27530d2fe4ec9237147589b84b
asdacosta <asdacosta18@gmail.com>
2024-10-29 15:45:21 +0000

    Define local strategy and deserialization for Passport.js

---
+++ src/controllers/control.ts
@@ -0,0 +1,29 @@
+import { Strategy as LocalStrategy } from "passport-local";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const localStrategy = new LocalStrategy(
+  { usernameField: "email" },
+  async (email, password, done) => {
+    try {
+      const user = await prisma.users.findUnique({ where: { email } });
+      if (!user || user.password !== password) return done(null, false);
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  }
+);
+
+const deserialize = async (id, done) => {
+  try {
+    const user = await prisma.users.findUnique({ where: { id } });
+    if (!user) return done(null, false);
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+};
+
+export { localStrategy, deserialize };
