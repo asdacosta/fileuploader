a88fe6485f3ffa6bfe2c2985c5f10cedaf5d0c24
asdacosta <asdacosta18@gmail.com>
2024-10-30 16:24:34 +0000

    Add deployment files

---
+++ dist/app.js
@@ -0,0 +1,102 @@
+import dotenv from "dotenv";
+import express from "express";
+import path from "node:path";
+import session from "express-session";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import { PrismaClient } from "@prisma/client";
+import passport from "passport";
+import {
+  localStrategy,
+  deserialize,
+  getHome,
+  signUpValidation,
+  postSignUp,
+  getLogOut,
+} from "./controllers/control.js";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import multer from "multer";
+dotenv.config();
+const app = express();
+const prisma = new PrismaClient();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const assetsPath = path.join(__dirname, "../public");
+const upload = multer({ dest: "../public/uploads/" });
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: false }));
+if (!process.env.SESSION_SECRET) throw new Error("Session secret not set.");
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000,
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
+app.use(passport.session());
+passport.use(localStrategy);
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser(deserialize);
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+app.get("/", getHome);
+app.get("/sign-up", (_req, res) => res.render("sign-up"));
+app.get("/log-in", (_req, res) => res.render("log-in"));
+app.get("/log-out", getLogOut);
+app.post("/sign-up", signUpValidation, postSignUp);
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/log-in",
+  })
+);
+app.post("/upload", upload.single("upload"), async (req, res) => {
+  if (!req.file) {
+    return res.status(400).render("index", {
+      user: req.user,
+      uploadMessage: "No file uploaded.",
+    });
+  }
+  if (!req.user) {
+    return res.status(401).render("index", {
+      user: null,
+      uploadMessage: "User not authenticated.",
+    });
+  }
+  const foundUser = await prisma.user.findUnique({
+    where: {
+      email: req.user.email,
+    },
+  });
+  if (!foundUser) {
+    return res.status(404).render("index", {
+      user: req.user,
+      uploadMessage: "User not found.",
+    });
+  }
+  await prisma.file.create({
+    data: {
+      userId: foundUser.id,
+      fileUrl: req.file.path,
+    },
+  });
+  res.render("index", {
+    user: req.user,
+    uploadMessage: "File uploaded successfully!",
+  });
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Ongoing on ${PORT} now!`);
+});
+//# sourceMappingURL=app.js.map
---
+++ dist/controllers/control.js
@@ -0,0 +1,113 @@
+import { Strategy as LocalStrategy } from "passport-local";
+import { PrismaClient } from "@prisma/client";
+import { validationResult, body } from "express-validator";
+import pkg from "bcryptjs";
+const { hash, compare } = pkg;
+const prisma = new PrismaClient();
+const signUpValidation = [
+    body("first_name")
+        .trim()
+        .notEmpty()
+        .withMessage("Name is required.")
+        .isAlpha()
+        .withMessage("Name must contain only letters."),
+    body("last_name")
+        .trim()
+        .notEmpty()
+        .withMessage("Name is required.")
+        .isAlpha()
+        .withMessage("Name must contain only letters."),
+    body("email")
+        .trim()
+        .isEmail()
+        .withMessage("Kindly enter a valid email.")
+        .custom(async (value) => {
+        const user = await prisma.user.findUnique({ where: { email: value } });
+        if (user)
+            throw new Error("Email already in use.");
+        return true;
+    })
+        .normalizeEmail(),
+    body("password")
+        .isLength({ min: 5 })
+        .withMessage("Password must be at least 5 chars.")
+        .matches(/\d/)
+        .withMessage("Password must contain a number.")
+        .matches(/[!@#$%^&*]/)
+        .withMessage("Password must contain special char.")
+        .escape(),
+    body("confirm_password").custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error("The passwords don't match.");
+        }
+        return true;
+    }),
+];
+const localStrategy = new LocalStrategy({ usernameField: "email" }, async (email, password, done) => {
+    try {
+        const user = await prisma.user.findUnique({ where: { email } });
+        if (!user)
+            return done(null, false, { message: "Incorrect username" });
+        const match = await compare(password, user.password);
+        if (!match)
+            return done(null, false, { message: "Incorrect password" });
+        return done(null, user);
+    }
+    catch (error) {
+        return done(error);
+    }
+});
+const deserialize = async (user, done) => {
+    try {
+        const foundUser = await prisma.user.findUnique({ where: { id: user.id } });
+        if (!foundUser)
+            return done(null, false);
+        done(null, foundUser);
+    }
+    catch (error) {
+        done(error);
+    }
+};
+const getHome = async (req, res) => {
+    return res.render("index", { user: req.user, uploadMessage: "" });
+};
+const postSignUp = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log("Error here!");
+        return res.render("sign-up", {
+            errors: errors.array(),
+            FormData: req.body,
+        });
+    }
+    try {
+        hash(req.body.password, 10, async (err, hashedPwd) => {
+            if (err)
+                return next(err);
+            await prisma.user.create({
+                data: {
+                    first_name: req.body.first_name,
+                    last_name: req.body.last_name,
+                    email: req.body.email,
+                    password: hashedPwd,
+                },
+            });
+        });
+        const user = await prisma.user.findUnique({
+            where: { email: req.body.email },
+        });
+        return res.render("index", { user, uploadMessage: "" });
+    }
+    catch (error) {
+        return next(error);
+    }
+};
+const getLogOut = (req, res, next) => {
+    req.logout((err) => {
+        if (err)
+            return next(err);
+        res.redirect("/");
+    });
+};
+export { localStrategy, deserialize, getHome, postSignUp, signUpValidation, getLogOut, };
+//# sourceMappingURL=control.js.map
